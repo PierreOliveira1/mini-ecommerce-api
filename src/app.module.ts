e5ddda927b6bc4532be091ae2a42fa3cf0208c3c
Pierre Oliveira <pierre.torres445@gmail.com>
2023-04-21 17:16:16 -0300

    feat: configured mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
@@ -9,6 +10,7 @@ import { ZodValidation } from './utils/zodvalidation.utils';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    MongooseModule.forRoot(process.env.DB_URI),
     ProductsModule,
   ],
   controllers: [AppController],
