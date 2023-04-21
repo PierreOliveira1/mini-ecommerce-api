c34440c7223f4f411ff35adeaa5112289dc9f641
Pierre Oliveira <pierre.torres445@gmail.com>
2023-04-21 19:57:40 -0300

    feat: add createdAt and updatedAt

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { v4 as uuidv4 } from 'uuid';
 
-@Schema()
+@Schema({ timestamps: true })
 export class Product {
   @Prop({ default: uuidv4 })
   _id: string;
@@ -17,6 +17,12 @@ export class Product {
 
   @Prop()
   image: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
