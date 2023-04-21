b8c33ddb0e38dd8cd1bca8a5d1a860e54d602c00
Pierre Oliveira <pierre.torres445@gmail.com>
2023-04-21 13:16:53 -0300

    feat: configured env and pipe zod validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
+import { APP_PIPE } from '@nestjs/core';
+import { ZodValidation } from './utils/zodvalidation.utils';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    ProductsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidation,
+    },
+  ],
 })
 export class AppModule {}
