a080bd818ceea94a4b638386914b4aa8d9dd93ae
Pierre Oliveira <pierre.torres445@gmail.com>
2023-04-21 17:17:19 -0300

    feat: create pagination dto

---
+++ src/products/dto/pagination.dto.ts
@@ -0,0 +1,16 @@
+import { createZodDto } from 'nestjs-zod';
+import { z } from 'nestjs-zod/z';
+
+const PaginationSchema = z.object({
+  page: z
+    .preprocess((value: string) => Number(value), z.number().min(0))
+    .optional(),
+  limit: z
+    .preprocess(
+      (value: string) => Number(value),
+      z.number().min(1).max(100).optional(),
+    )
+    .optional(),
+});
+
+export class PaginationDto extends createZodDto(PaginationSchema) {}
