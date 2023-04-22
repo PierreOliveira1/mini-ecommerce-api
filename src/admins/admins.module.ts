b4a634c2c9382e36398de11f80038b84b0da13b5
Pierre Oliveira <pierre.torres445@gmail.com>
2023-04-21 23:36:16 -0300

    feat: module admin

---
+++ src/admins/admins.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AdminsService } from './admins.service';
+import { AdminsController } from './admins.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Admin, AdminSchema } from './entities/admin.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }]),
+  ],
+  controllers: [AdminsController],
+  providers: [AdminsService],
+})
+export class AdminsModule {}
