3fed7d3ace519ad5b403c1f6215a3f83c2b71b5a
Pierre Oliveira <pierre.torres445@gmail.com>
2023-04-21 23:36:32 -0300

    feat: create schema admin

---
+++ src/admins/entities/admin.entity.ts
@@ -0,0 +1,39 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { v4 as uuidv4 } from 'uuid';
+import * as bcrypt from 'bcryptjs';
+
+@Schema({ timestamps: true })
+export class Admin {
+  @Prop({ default: uuidv4 })
+  _id: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
+
+  comparePassword(password: string): boolean {
+    return bcrypt.compareSync(password, this.password);
+  }
+}
+
+export const AdminSchema = SchemaFactory.createForClass(Admin);
+
+AdminSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
+});
