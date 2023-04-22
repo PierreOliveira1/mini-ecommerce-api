15a6327d4095a6bdf81fe4c8eff9d22effa9f28f
Pierre Oliveira <pierre.torres445@gmail.com>
2023-04-21 23:37:36 -0300

    feat: add admin module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { APP_PIPE } from '@nestjs/core';
 import { ZodValidation } from './utils/zodvalidation.utils';
+import { AdminsModule } from './admins/admins.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     MongooseModule.forRoot(process.env.DB_URI),
     ProductsModule,
+    AdminsModule,
   ],
   controllers: [AppController],
   providers: [
