6a384441d5dfe13728e1e718716138146ed3cb60
Pierre Oliveira <pierre.torres445@gmail.com>
2023-04-23 11:49:58 -0300

    feat: configured jwt

--- src/admins/admins.module.ts
+++ src/admins/admins.module.ts
@@ -3,12 +3,26 @@ import { AdminsService } from './admins.service';
 import { AdminsController } from './admins.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Admin, AdminSchema } from './entities/admin.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
     MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }]),
   ],
   controllers: [AdminsController],
-  providers: [AdminsService],
+  providers: [AdminsService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AdminsModule {}
