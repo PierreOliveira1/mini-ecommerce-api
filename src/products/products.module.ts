3c2944eed4d385b5bbb4f464c2d0ac42b82a17a2
Pierre Oliveira <pierre.torres445@gmail.com>
2023-04-23 11:41:16 -0300

    feat: import module authadmin

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,10 +3,12 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from './entities/product.entity';
+import { AdminsModule } from '@admins/admins.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    AdminsModule,
   ],
   controllers: [ProductsController],
   providers: [ProductsService],
